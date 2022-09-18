6cd408df514f0d8ffc583f8d5d09a2391280285c
kerkazou <kerkazou.zakaria@gmail.com>
2022-09-18 01:09:05 +0100

    Foother

--- src/components/Services.js
+++ src/components/Services.js
@@ -3,8 +3,40 @@ import React, { Component } from 'react';
 class Services extends Component {
   render() {
     return (
-      <div id='services'>
+      <div className='gap-5' id='services'>
         <div id='title'>Services</div>
+        <div className='w-100 d-flex flex-wrap justify-content-center align-items-center gap-4'>
+          <div className='col-md-3 col-sm-5 col-10 d-flex flex-column justify-content-center align-items-center services-cart'>
+            <i className="bi bi-code-slash"></i>
+            <div>Web Design</div>
+            <div>Discription services</div>
+          </div>
+          <div className='col-md-3 col-sm-5 col-10 d-flex flex-column justify-content-center align-items-center services-cart'>
+            <i className="bi bi-code-slash"></i>
+            <div>Web Design</div>
+            <div>Discription services</div>
+          </div>
+          <div className='col-md-3 col-sm-5 col-10 d-flex flex-column justify-content-center align-items-center services-cart'>
+            <i className="bi bi-code-slash"></i>
+            <div>Web Design</div>
+            <div>Discription services</div>
+          </div>
+          <div className='col-md-3 col-sm-5 col-10 d-flex flex-column justify-content-center align-items-center services-cart'>
+            <i className="bi bi-code-slash"></i>
+            <div>Web Design</div>
+            <div>Discription services</div>
+          </div>
+          <div className='col-md-3 col-sm-5 col-10 d-flex flex-column justify-content-center align-items-center services-cart'>
+            <i className="bi bi-code-slash"></i>
+            <div>Web Design</div>
+            <div>Discription services</div>
+          </div>
+          <div className='col-md-3 col-sm-5 col-10 d-flex flex-column justify-content-center align-items-center services-cart'>
+            <i className="bi bi-code-slash"></i>
+            <div>Web Design</div>
+            <div>Discription services</div>
+          </div>
+        </div>
       </div>
     );
   }
